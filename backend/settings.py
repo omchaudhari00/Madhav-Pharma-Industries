20fbca67b80dfcfda879345a5351eeeb1b1b4859
omchaudhari00 <omchaudhari2627@gmail.com>
2026-08-08 17:17:56 +0530

    Fix OTP 500 crash by adding EMAIL_TIMEOUT

--- backend/settings.py
+++ backend/settings.py
@@ -165,6 +165,7 @@ EMAIL_BACKEND = os.getenv('EMAIL_BACKEND', 'django.core.mail.backends.smtp.Email
 EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
 EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True').lower() in ('true', '1', 'yes')
+EMAIL_TIMEOUT = 5
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', f'Madhav Pharma Industries <{EMAIL_HOST_USER}>')
